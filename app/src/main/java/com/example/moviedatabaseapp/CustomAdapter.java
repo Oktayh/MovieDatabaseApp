cd05fbde3dc44cde9b095269ac7ba8670f84ab8d
Oktay HIZARCI <oktayhizarci@gmail.com>
2021-04-19 03:43:19 +0300

    UI updated.

--- app/src/main/java/com/example/moviedatabaseapp/CustomAdapter.java
+++ app/src/main/java/com/example/moviedatabaseapp/CustomAdapter.java
@@ -1,23 +1,30 @@
 package com.example.moviedatabaseapp;
 
 import android.content.Context;
+import android.graphics.drawable.GradientDrawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+
+import static java.security.AccessController.getContext;
 
 public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.CustomViewHolder> {
 
     private List<CustomModel> models;
+    Context context;
 
-    public CustomAdapter(List<CustomModel> models) {
-
+    public CustomAdapter( Context context, List<CustomModel> models) {
+        this.context = context;
         this.models = models;
     }
 
@@ -35,6 +42,11 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.CustomView
         holder.movie_name.setText(models.get(position).getName());
         holder.movie_genre.setText(models.get(position).getGenra());
         holder.movie_year.setText(String.valueOf(models.get(position).getYear()));
+        GradientDrawable magnitudeCircle = (GradientDrawable) holder.movie_img.getBackground();
+        // Get the appropriate background color based on the current earthquake magnitude
+        int Color = getColor(context);
+        // Set the color on the magnitude circle
+        magnitudeCircle.setColor(Color);
 
     }
 
@@ -45,13 +57,42 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.CustomView
 
     public class CustomViewHolder extends RecyclerView.ViewHolder {
 
-        TextView movie_name,movie_genre,movie_year;
+        TextView movie_name,movie_genre,movie_year,movie_img;
         public CustomViewHolder(@NonNull View itemView) {
             super(itemView);
             movie_name =(TextView) itemView.findViewById(R.id.txt_name);
             movie_genre = (TextView)itemView.findViewById(R.id.txt_genre);
             movie_year = (TextView)itemView.findViewById(R.id.txt_year);
+            movie_img = (TextView) itemView.findViewById(R.id.imageView2);
 
         }
+    }
+    private int getColor(Context context) {
+
+        int ColorResourceId;
+        Random rn = new Random();
+        int answer = rn.nextInt(6);
+        switch (answer) {
+            case 0:
+                ColorResourceId = R.color.color_circle5;
+                break;
+            case 1:
+                ColorResourceId = R.color.color_circle1;
+                break;
+            case 2:
+                ColorResourceId = R.color.color_circle2;
+                break;
+            case 3:
+                ColorResourceId = R.color.color_circle3;
+                break;
+            case 4:
+                ColorResourceId = R.color.color_circle4;
+                break;
+            default:
+                ColorResourceId = R.color.purple_200;
+                break;
+        }
+
+        return ContextCompat.getColor(context,ColorResourceId);
     }
 }
